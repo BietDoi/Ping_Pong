c86d1b540ff3c125f1b0f98351e46eccb3799698
duoc.nguyen <duoc.nguyen@mtech.com.vn>
2020-04-21 09:18:49 +0700

    ping pong game

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1587435164,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/side-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/playerbar-sheet0.png",
+		"images/computerbar-sheet0.png",
+		"images/line_center-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
